c1c844904c1c0070ce9bd440c1818ac05ee87c32
Roberto Ruiz <rroberto.rruiz@gmail.com>
2018-09-05 12:58:56 -0500

    Initial project setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# new-marias
-Proyecto de New Marías, desarrollado con Gatsby V2 y Styled Components
---
+++ src/components/layout.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components';
+
+import './setup.css';
+
+const ChildrenLayout = styled.section`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`;
+
+function Layout({children}) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => (
+        <React.Fragment>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Sample' },
+              { name: 'keywords', content: 'sample, something' },
+            ]}
+          >
+            <html lang="en" />
+          </Helmet>
+          <ChildrenLayout>
+            {children}
+          </ChildrenLayout>
+        </React.Fragment>
+      )}
+    />
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
